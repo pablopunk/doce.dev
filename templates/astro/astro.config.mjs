449a9e213e28a3b560b9d01b6905d3b8f0ebcb00
Pablo P Varela <pablo@pablopunk.com>
2025-11-11 19:14:58 +0100

    0 errors template

--- templates/astro/astro.config.mjs
+++ templates/astro/astro.config.mjs
@@ -1,7 +1,12 @@
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
 	integrations: [react()],
 	vite: {
 		css: {
