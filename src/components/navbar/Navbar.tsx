519d5a90638e7b8c94ecc4b849ef855a48d273ff
Pablo P Varela <pablo@pablopunk.com>
2026-01-08 18:56:36 +0100

    test: remove icon

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -56,7 +56,6 @@ export function Navbar() {
 						href="/"
 						className="flex items-center font-semibold text-sm tracking-tight gap-2"
 					>
-						<img src="/icon-1080.svg" alt="doce.dev" className="w-5 h-5" />
 						<span>
 							doce<span className="text-muted-foreground">.dev</span>
 						</span>
