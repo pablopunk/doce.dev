b34faffb95a0e42cbda9fc7c6ff248c09b56c530
Pablo P Varela <pablo@pablopunk.com>
2026-02-17 16:12:43 +0100

    stop at end of gif

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -3,16 +3,16 @@ import { Composition } from "remotion";
 import { DoceShowcase } from "./DoceShowcase";
 
 export const RemotionRoot: React.FC = () => {
-  return (
-    <>
-      <Composition
-        id="DoceShowcase"
-        component={DoceShowcase}
-        durationInFrames={555}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-    </>
-  );
+	return (
+		<>
+			<Composition
+				id="DoceShowcase"
+				component={DoceShowcase}
+				durationInFrames={615}
+				fps={30}
+				width={1920}
+				height={1080}
+			/>
+		</>
+	);
 };
